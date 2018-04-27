81ae7cbd4527a2c8353c7fb376cf4a1eb860a8b6
softtek.adrianok <adriano.kanesaki@softtek.com>
2018-04-27 17:59:05 -0300

    initial code.

---
+++ TaskService/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+using System.Web.Cors;
+
+namespace TaskService
+{
+    public partial class Startup
+    {
+        // The OWIN middleware will invoke this method when the app starts
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureCors(app);
+
+            //app.UserCors();
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureCors(IAppBuilder app)
+        {
+            var politica = new CorsPolicy();
+
+            politica.AllowAnyHeader = true;
+            politica.AllowAnyMethod = true;
+            politica.AllowAnyOrigin = true;
+
+            //politica.Origins.Add("http://localhost:40874");
+            //politica.Origins.Add("http://localhost:40880");
+
+            politica.Methods.Add("GET");
+            politica.Methods.Add("POST");
+
+            var corsOptions = new CorsOptions
+            {
+                PolicyProvider = new CorsPolicyProvider
+                {
+                    PolicyResolver = context => Task.FromResult(politica)
+                }
+            };
+
+            app.UseCors(corsOptions);
+        }
+
+    }
+}
